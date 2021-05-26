5c20a66c4ba97a3cde6c1196784e66ff63c566d8
Jason Steffener - laptop <steffener@gmail.com>
2021-05-26 15:29:23 -0400

    checked AES

--- Questionnaires/assets/aes_setup_eng.js
+++ Questionnaires/assets/aes_setup_eng.js
@@ -34,3 +34,5 @@ var items = [
 var instructions = 'For each statement, choose the answer that best describes the your thoughts, feelings, and activity in the past 4 weeks.'
 
 var references = "Marin, R. S., Biedrzycki, R. C., & Firinciogullari, S. (1991). Reliability and validity of the Apathy Evaluation Scale. Psychiatry research, 38(2), 143-162."
+
+var notes = "The Apathy Evaluation Scale (AES) (46): Apathy was quantified using the AES, consisting of 18 items relating to apathy, each scored on a 4-point Likert-type scale. The score range for the complete AES (total AES score) is 18 to 72, with a lower score indicating greater apathy."
