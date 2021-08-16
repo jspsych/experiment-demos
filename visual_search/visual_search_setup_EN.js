a7054da05fde3e3c13e7a03933a316aaf80256ae
Peter J. Kohler <pjkohl3r@gmail.com>
2021-08-16 13:49:38 -0400

    better control of prompt targets

--- visual_search/visual_search_setup_EN.js
+++ visual_search/visual_search_setup_EN.js
@@ -10,7 +10,7 @@ const shape_target = ['assets/images/greenS.png', 'assets/images/redS.png']
 const colour_target = ['assets/images/blueL.png', 'assets/images/blueT.png']
 const foil_images = ['assets/images/greenL.png', 'assets/images/redT.png'] 
 const fixation = 'assets/images/blackDot.png' //fixation
-const experiment_type = 'conjunction' //can also take the value 'shape' and 'colour'
+const experiment_type = 'colour' //can also take the value 'shape' and 'colour'
 
 //keys to press
 const the_present_key = 'j'
@@ -25,7 +25,8 @@ const circle_diam = 0 //if set at 0, circle diameter will be calculated based st
 const prompt_msg = "Keep staring at the black dot in the center of the screen. <br>" +
                    "Use the j and f keys to indicate whether or not the target is present. <br>" +
                    "<strong>If the target is present press j. If not press f.</strong><br>"+
-                   "Below you will see an example of the target image." 
+                   "There can be one or more target images.<br>"+
+                   "Below you will see an example of the target image(s)." 
 
 const prac_msg = "There will be $PRAC_LEN practice trials before the experiment begins. Press any key to start."
 
